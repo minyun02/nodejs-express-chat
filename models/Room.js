1b6ad3b2a8e61560258316069edc567c8292a93a
ArchivvonJang <76926509+ArchivvonJang@users.noreply.github.com>
2022-07-03 18:05:31 +0900

    ♻️  refactor code

--- models/Room.js
+++ models/Room.js
@@ -1,27 +1,18 @@
 const mongoose = require('mongoose');
 
 const roomSchema = mongoose.Schema({
-    room_num : {
-        type : Number
-    },
+    room_num : Number,
     room_name : {
         type : String,
         maxlength : 100
     },
-    max_participants : {
-        type : Number
-    },
-    owner : {
-        type : user_num
-    },
-    number_participants : {
-        type : Number
-    },
+    max_participants : Number,
+    owner : user_num,
+    number_participants : Number,
     reg_date : {
         type : Date,
         default: Date.now
     }
-
 })
 
 const User = mongoose.model('Room', RoomSchema)
