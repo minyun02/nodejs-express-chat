0c5143be3da9a660f6a371b4beb5dfda1158b6a5
ArchivvonJang <76926509+ArchivvonJang@users.noreply.github.com>
2022-07-31 22:01:06 +0900

    새로운 브랜치에서 작업하기 전 남겨놓기

--- index.js
+++ index.js
@@ -1,31 +1,96 @@
-require('dotenv').config();
+import express from 'express';
+import { createServer } from 'http';
+import { instrument } from "@socket.io/admin-ui";
+import { Server } from 'socket.io';
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import path from 'path';
+
+const __dirname = path.resolve();
+
+dotenv.config();
 const {MONGODB_URI} = process.env;
-const mongoose = new require('mongoose');
-const express = require('express');
+
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require('socket.io');
-const io = new Server(server);
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
 
 mongoose.connect(MONGODB_URI)
 .then(() => { console.log('MongoDB Connected successfully') })
 .catch(err => console.log(err));
 
+app.set("views", __dirname + "/views");
 app.use(express.static(__dirname + '/src'));
 
 let userId = '';
 
 app.get('/', (req, res) => {
-  console.log(__dirname)
+  console.log(__dirname);
   res.sendFile(__dirname + '/src/views/index.html');
+  app.get("/", (_, res) => res.render("home"));
+  app.get("/*", (_, res) => res.redirect("/"));
 });
 
+const ioServer = new Server(httpServer, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true
+  }
+});
+
+instrument(ioServer, {
+  auth: false
+});
+
+
+function publicRooms(){
+  const { sockets: { adapter: { sids, rooms }, },} = io;
+
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if(sids.get(key) === undefined){
+      publicRooms.push(key);
+    }
+  });
+  return publicRooms;
+}
+
+function countRoom(roomName){
+  return io.sockets.adapter.rooms.get(roomName)?.size;
+}
+
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnection', () => {
-        console.log('user disconnected');
+    console.log('[Server] user connected');
+
+    //새로운 회원 입장 시 전체 메시지
+    socket["nickname"] = "Anonymous";
+    socket.onAny((event) => {
+      console.log(`socket event : ${event}`);
     });
+
+    socket.on("enter_room", (roomName, done) => {
+      socket.join(roomName);
+      done();
+      //메세지를 하나의 socket에만 보내기
+      socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
+      //메시지를 모든 socket에 보내주기
+      io.sockets.emit("room_change", publicRooms());
+    });
+
+    //클라이언트가 서버와 연결이 끊어지기 전에 굿바이 메시지 
+    socket.on("disconnecting", () => {
+      console.log('[Server] user disconnected');
+      socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname, countRoom(room)- 1 ));
+    });
+
+    socket.on("new_message", (msg, room, done) => {
+      socket.to(room).emit("new_message", `${socket.nickname} : ${msg}`);
+      done();
+    });
+
+    socket.on("nickname", nickname => socket["nickname"] = nickname);
+
     socket.on('chat message', (msg, id) => {
       userId = id;
       console.log('message: ' + msg + ', ' + id);
@@ -33,6 +98,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
+httpServer.listen(3000, () => {
   console.log('listening on *:3000');
 });
--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,15 +1,104 @@
-let userId = prompt('userid?');
-let socket = io();
+//let userId = prompt('userid?');
+import { io } from "socket.io-client";
+const socket = io();
 
-const form = document.getElementById('form');
-const input = document.getElementById('input');
+const messageForm = document.getElementById('messageForm');
+const message = document.getElementById('message');
+const chatRoom = document.getElementById("chatRoom");
+const welcome = document.getElementById("welcome");
+const newRoom = document.getElementById("roomName");
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    if (input.value) {
-        socket.emit('chat message', input.value, userId);
-        input.value = '';
+// chatRoom.hidden = true;
+let roomName;
+
+function addMessage(message){
+    const listUl = room.querySelector("#list");
+    console.log(listUlper.innerHTML);
+    const listLi = document.createElement("li");
+    listLi.innerText(message);
+    ul.appendChild(listLi);
+}
+
+//메시지 보내기 function
+function handleMessageSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector("#messageForm input");
+    const value = input.value;
+    //입력받은 메시지를 back-end로 보내준다.
+    socket.emit("new_message", input.value, roomName, () => {
+        addMessage(`You : ${value}`); //백엔드에서 시작시킬 수 있는 function
+    });
+    input.value = "";
+}
+
+//닉네임 정하기 function
+function handleNicknameSubmit(event) {
+    event.preventDefault();
+    const input = room.querySelector("#nameform input");
+    socket.emit("nickname", input.value);
+}
+
+function showRoom(){
+    welcome.hidden = true;
+    room.hidden = false;
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName}`;
+    const messageForm = room.querySelector("#messageForm");
+    const nameForm = room.querySelector("#nameForm");
+    messageForm.addEventListener("submit", handleMessageSubmit);
+    nameForm.addEventListener("submit", handleNicknameSubmit);
+};
+
+function handleRoomSubmit(event){
+    event.preventDefault();
+    const input = form.querySelector("roomName");
+     //argument ->  1. 이벤트이름, 2. 보내고 싶은 payload, 3. 서버에서 호출하는 function
+    socket.emit(
+      "enter_room",
+      input.value, //방 이름
+      showRoom//function - backend에서 끝났다는 사실을 알리기 위한 function은 맨 마지막 arg가 되어야한다.
+    );
+    roomName = input.value;
+    input.value="";
+    // 1. 특정한 event 를 어떤 이름이든 상관없이 emit 해줄 수 있다.
+    // 2. JS object 를 전송할 수 있다.
+};
+
+form.addEventListener("submit", handleRoomSubmit);
+// form.addEventListener('submit', function(e) {
+//     e.preventDefault();
+//     if (input.value) {
+//         socket.emit('chat message', input.value, userId);
+//         input.value = '';
+//     }
+// });
+//새로운 참여자가 room에 들어오면 수행될 function
+socket.on("welcome", (user, newCount) => {
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName} (${newCount})`;
+    addMessage(`[ ${user} arrived. ]`);
+});
+//참여자가 방을 나가면 수행하게 될 function
+socket.on("bye", (left, newCount) => {
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName} (${newCount})`;
+    addMessage(`[ ${left} left. ]`);
+});
+//메시지 확인
+socket.on("new_message", addMessage);
+
+socket.on("room_change", (rooms) => {
+    roomList.innerHTML = '';
+    //room이 하나도 없을 때, 모든 것을 비워준다.
+    if(room.length === 0){
+      return;
     }
+    const roomList = welcome.querySelector("ul");
+    rooms.forEach(room => {
+      const li = document.createElement("li");
+      li.innerText = room;
+      roomList.append(li);
+    })
 });
 
 socket.on('chat message', function(msg, id) {
