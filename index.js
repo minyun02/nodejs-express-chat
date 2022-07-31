26b525f8b18e4852ef1611ba88ba37489964b22e
min <min52@malgnsoft.com>
2022-07-31 23:37:11 +0900

    회원가입 - db 저장

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const {MONGODB_URI} = process.env;
 const mongoose = new require('mongoose');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
@@ -12,13 +13,40 @@ mongoose.connect(MONGODB_URI)
 .then(() => { console.log('MongoDB Connected successfully') })
 .catch(err => console.log(err));
 
+let UserSchema = new mongoose.Schema({
+  userid : String,
+  password : String
+});
+
+let Users = mongoose.model('Users', UserSchema);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ limit : '1gb', extended : false}));
 app.use(express.static(__dirname + '/src'));
 
 let userId = '';
 
 app.get('/', (req, res) => {
   console.log(__dirname)
-  res.sendFile(__dirname + '/src/views/index.html');
+  // res.sendFile(__dirname + '/src/views/index.html');
+  res.sendFile(__dirname + '/src/views/login.html');
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+});
+
+app.get('/signup', (req, res) => {
+  res.sendFile(__dirname + '/src/views/signup.html');
+});
+
+app.post('/signup', (req, res) => {
+  const user = new Users(req.body);
+  console.log(req.body)
+  user.save((err) => {
+    if(err) return res.status(500).json({message : "저장 실패"});
+    else return res.status(200).json({message : "저장 성공", data : user});
+  })
 });
 
 io.on('connection', (socket) => {
